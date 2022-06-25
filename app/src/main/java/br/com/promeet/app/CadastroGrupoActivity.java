6a3402c27c63627693b0a0958eef00f3c5a66ed8
Murilo Andrade <murilo.andrade@gmail.com>
2022-06-25 11:54:28 -0300

    Criadas todas as activities que serão usadas

---
+++ app/src/main/java/br/com/promeet/app/CadastroGrupoActivity.java
@@ -0,0 +1,14 @@
+package br.com.promeet.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroGrupoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_grupo);
+    }
+}
