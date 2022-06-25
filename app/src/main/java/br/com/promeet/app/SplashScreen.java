44ee8af9a8dfbc710377188d08245573c787b165
Murilo Andrade <murilo.andrade@gmail.com>
2022-06-24 22:12:14 -0300

    Splash "full screen", ajustes de configuração de fontes e "min api level" definido pra 28

---
+++ app/src/main/java/br/com/promeet/app/SplashScreen.java
@@ -0,0 +1,27 @@
+package br.com.promeet.app;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        Window window = this.getWindow();
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        // finally change the color
+        window.setStatusBarColor(ContextCompat.getColor(this,R.color.branco));
+    }
+}
