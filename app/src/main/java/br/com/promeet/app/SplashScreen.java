5360733eea34c477dc79d2412ca8796869733173
Murilo Andrade <murilo.andrade@gmail.com>
2022-07-01 22:58:46 -0300

    Atualização da SplashScreen

--- app/src/main/java/br/com/promeet/app/SplashScreen.java
+++ app/src/main/java/br/com/promeet/app/SplashScreen.java
@@ -1,9 +1,13 @@
 package br.com.promeet.app;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -17,7 +21,11 @@ public class SplashScreen extends AppCompatActivity {
         actionBar.hide();
         Window window = this.getWindow();
         // clear FLAG_TRANSLUCENT_STATUS flag:
-        //window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false);
+        } else {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
         // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         // finally change the color
